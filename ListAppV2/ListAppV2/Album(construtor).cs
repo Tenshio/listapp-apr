5445717a9d9fa4bc79fb751cf9bb0e7cbb64a7a4
Nicolas Herbert Mietzsch <96891308+Tenshio@users.noreply.github.com>
2022-03-22 09:52:41 -0300

    Add files via upload

---
+++ ListAppV2/ListAppV2/Album(construtor).cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAppV2
+{
+
+
+    public class Album
+    {
+        public string _name { get; set; }
+        public string _composer { get; set; }
+        public DateTime _Release_date { get; set; }
+        public int _track_count { get; set; }
+        public Album()
+        {
+        }
+
+        public Album(string name, string composer)
+        {
+            _name = name;
+            _composer = composer;
+        }
+        public Album(string name, string composer, int track_count)
+        {
+            _name = name;
+            _composer = composer;
+            _track_count = track_count;
+        }
+
+        //print dos albuns
+        public static void Listacw()
+        {
+
+            List<Album> Albuns = Lista.ListAlbum();
+            Console.WriteLine(" \n Short by album name \n");
+            foreach (var x in Albuns.OrderBy(x => x._name))
+            {
+                Console.WriteLine($"Album: { x._name}    Composer: {x._composer}");
+            }
+            Console.WriteLine(" \n Short by album composer \n");
+            foreach (var x in Albuns.OrderBy(x => x._composer))
+            {
+                Console.WriteLine($"Album: { x._name}    Composer: {x._composer}");
+            }
+            Console.WriteLine(" \n Short by album track number \n");
+            foreach (var x in Albuns.OrderByDescending(x => x._track_count))
+            {
+                if (x._track_count > 0)
+                {
+                    Console.WriteLine($"Album: { x._name}    Composer: {x._composer}     N' of tracks: {x._track_count}");
+                }
+
+            }
+
+        }
+
+
+
+
+
+    }
+
+}
