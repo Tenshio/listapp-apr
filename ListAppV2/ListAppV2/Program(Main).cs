c640b4c5d956a3fd7b892dcfdb2755f342ef1671
Nicolas Herbert Mietzsch <96891308+Tenshio@users.noreply.github.com>
2022-03-22 09:55:16 -0300

    Add files via upload

---
+++ ListAppV2/ListAppV2/Program(Main).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAppV2
+{
+
+    internal class Program : Album
+    {
+
+        public static void Main(string[] args)
+        {
+            Album.Listacw();
+            Console.ReadKey();
+        }
+    }
+}
